a34ac10269d3ecf202b73175598730b3e06d2e4d
Nux007 <007.nux@gmail.com>
2020-03-10 13:23:02 +0100

    Activite AND Classe comments, functions names changes and spacing.

--- src/Entity/Activite.php
+++ src/Entity/Activite.php
@@ -6,11 +6,13 @@ use Doctrine\Common\Collections\ArrayCollection;
 use Doctrine\Common\Collections\Collection;
 use Doctrine\ORM\Mapping as ORM;
 
+
 /**
  * @ORM\Entity(repositoryClass="App\Repository\ActiviteRepository")
  */
 class Activite
 {
+
     /**
      * @ORM\Id()
      * @ORM\GeneratedValue()
@@ -66,81 +68,156 @@ class Activite
      */
     private $notes;
 
+
+    /**
+     * Activite constructor.
+     */
     public function __construct()
     {
         $this->notes = new ArrayCollection();
     }
 
+
+    /**
+     * Return the Activity Id.
+     * @return int|null
+     */
     public function getId(): ?int
     {
         return $this->id;
     }
 
-    public function getIdNoteType(): ?NoteType
+
+    /**
+     * Return the activity NoteType.
+     * @return NoteType|null
+     */
+    public function getNoteType(): ?NoteType
     {
         return $this->idNoteType;
     }
 
-    public function setIdNoteType(?NoteType $idNoteType): self
+
+    /**
+     * Set the activity NoteType
+     * @param NoteType|null $noteType
+     * @return $this
+     */
+    public function setNoteType(?NoteType $noteType): self
     {
-        $this->idNoteType = $idNoteType;
+        $this->idNoteType = $noteType;
 
         return $this;
     }
 
-    public function getIdKnowledge(): ?Knowledge
+
+    /**
+     * Return the knowledge associated to the activity OR null if no matiere assigned.
+     * @return Knowledge|null
+     */
+    public function getKnowledge(): ?Knowledge
     {
         return $this->idKnowledge;
     }
 
-    public function setIdKnowledge(?Knowledge $idKnowledge): self
+
+    /**
+     * Set the knowledge if applicable ( knowledge or matiere ).
+     * @param Knowledge|null $knowledge
+     * @return $this
+     */
+    public function setKnowledge(?Knowledge $knowledge): self
     {
-        $this->idKnowledge = $idKnowledge;
+        $this->idKnowledge = $knowledge;
 
         return $this;
     }
 
-    public function getIdMatiere(): ?Matiere
+
+    /**
+     * Return the matiere if applicable OR null if knowledge assigned.
+     * @return Matiere|null
+     */
+    public function getMatiere(): ?Matiere
     {
         return $this->idMatiere;
     }
 
-    public function setIdMatiere(?Matiere $idMatiere): self
+
+    /**
+     * Set the matiere if applicable ( knowledge or matiere ).
+     * @param Matiere|null $matiere
+     * @return $this
+     */
+    public function setMatiere(?Matiere $matiere): self
     {
-        $this->idMatiere = $idMatiere;
+        $this->idMatiere = $matiere;
 
         return $this;
     }
 
-    public function getIdUser(): ?User
+
+    /**
+     * Return the User who created the activity.
+     * @return User|null
+     */
+    public function getUser(): ?User
     {
         return $this->idUser;
     }
 
-    public function setIdUser(?User $idUser): self
+
+    /**
+     * Set the user who created the activity.
+     * @param User|null $user
+     * @return $this
+     */
+    public function setUser(?User $user): self
     {
-        $this->idUser = $idUser;
+        $this->idUser = $user;
 
         return $this;
     }
 
-    public function getIdPeriode(): ?Periode
+
+    /**
+     * Return the Period attached to the activity.
+     * @return Periode|null
+     */
+    public function getPeriode(): ?Periode
     {
         return $this->idPeriode;
     }
 
-    public function setIdPeriode(?Periode $idPeriode): self
+
+    /**
+     * Set the period attache to the activity.
+     * @param Periode|null $periode
+     * @return $this
+     */
+    public function setPeriode(?Periode $periode): self
     {
-        $this->idPeriode = $idPeriode;
+        $this->idPeriode = $periode;
 
         return $this;
     }
 
+
+    /**
+     * Return true if this actifity MUST be included in period calculations, false otherwise.
+     * @return bool|null
+     */
     public function getActiveInPeriod(): ?bool
     {
         return $this->activeInPeriod;
     }
 
+
+    /**
+     * Set to true in order to use activity score in period, false otherwise.
+     * @param bool $activeInPeriod
+     * @return $this
+     */
     public function setActiveInPeriod(bool $activeInPeriod): self
     {
         $this->activeInPeriod = $activeInPeriod;
@@ -148,11 +225,22 @@ class Activite
         return $this;
     }
 
+
+    /**
+     * Return the activity global comment or description.
+     * @return string|null
+     */
     public function getComment(): ?string
     {
         return $this->comment;
     }
 
+
+    /**
+     * Set the activity global comment or description.
+     * @param string|null $comment
+     * @return $this
+     */
     public function setComment(?string $comment): self
     {
         $this->comment = $comment;
@@ -160,11 +248,22 @@ class Activite
         return $this;
     }
 
+
+    /**
+     * Return the activity name / title.
+     * @return string|null
+     */
     public function getName(): ?string
     {
         return $this->name;
     }
 
+
+    /**
+     * Set the activity name / title.
+     * @param string $name
+     * @return $this
+     */
     public function setName(string $name): self
     {
         $this->name = $name;
@@ -172,7 +271,9 @@ class Activite
         return $this;
     }
 
+
     /**
+     * Return the Note collection attributed to User
      * @return Collection|Note[]
      */
     public function getNotes(): Collection
@@ -180,6 +281,12 @@ class Activite
         return $this->notes;
     }
 
+
+    /**
+     * Add a note to the activity.
+     * @param Note $note
+     * @return $this
+     */
     public function addNote(Note $note): self
     {
         if (!$this->notes->contains($note)) {
@@ -190,6 +297,12 @@ class Activite
         return $this;
     }
 
+
+    /**
+     * Remove a note from the activity.
+     * @param Note $note
+     * @return $this
+     */
     public function removeNote(Note $note): self
     {
         if ($this->notes->contains($note)) {
--- src/Entity/Classe.php
+++ src/Entity/Classe.php
@@ -6,6 +6,7 @@ use Doctrine\Common\Collections\ArrayCollection;
 use Doctrine\Common\Collections\Collection;
 use Doctrine\ORM\Mapping as ORM;
 
+
 /**
  * @ORM\Entity(repositoryClass="App\Repository\ClasseRepository")
  */
@@ -39,6 +40,10 @@ class Classe
      */
     private $eleves;
 
+
+    /**
+     * Classe constructor.
+     */
     public function __construct()
     {
         $this->users = new ArrayCollection();
@@ -50,11 +55,22 @@ class Classe
         return $this->id;
     }
 
+
+    /**
+     * Return the classe name.
+     * @return string|null
+     */
     public function getName(): ?string
     {
         return $this->name;
     }
 
+
+    /**
+     * Set the classe name.
+     * @param string $name
+     * @return $this
+     */
     public function setName(string $name): self
     {
         $this->name = $name;
@@ -62,19 +78,32 @@ class Classe
         return $this;
     }
 
-    public function getIdTypeClasse(): ?TypeClasse
+
+    /**
+     * Return the classe TypeClasse.
+     * @return TypeClasse|null
+     */
+    public function getTypeClasse(): ?TypeClasse
     {
         return $this->idTypeClasse;
     }
 
-    public function setIdTypeClasse(?TypeClasse $idTypeClasse): self
+
+    /**
+     * Set the classe type classe.
+     * @param TypeClasse|null $typeClasse
+     * @return $this
+     */
+    public function setTypeClasse(?TypeClasse $typeClasse): self
     {
-        $this->idTypeClasse = $idTypeClasse;
+        $this->idTypeClasse = $typeClasse;
 
         return $this;
     }
 
+
     /**
+     * Return the available Classe Users ( eg: Prof, Special master, ... all allowed users to manage the Class ).
      * @return Collection|User[]
      */
     public function getUsers(): Collection
@@ -82,6 +111,12 @@ class Classe
         return $this->users;
     }
 
+
+    /**
+     * Enable a user to manage this Class by adding it to the object users list.
+     * @param User $user
+     * @return $this
+     */
     public function addUser(User $user): self
     {
         if (!$this->users->contains($user)) {
@@ -92,6 +127,12 @@ class Classe
         return $this;
     }
 
+
+    /**
+     * Removes a user from allowed users for the Classe.
+     * @param User $user
+     * @return $this
+     */
     public function removeUser(User $user): self
     {
         if ($this->users->contains($user)) {
@@ -102,7 +143,9 @@ class Classe
         return $this;
     }
 
+
     /**
+     * Return a Collection of Eleve objects registered to the Class object.
      * @return Collection|Eleve[]
      */
     public function getEleves(): Collection
@@ -110,21 +153,33 @@ class Classe
         return $this->eleves;
     }
 
-    public function addElefe(Eleve $elefe): self
+
+    /**
+     * Add an Eleve object to the Classe object.
+     * @param Eleve $eleve
+     * @return $this
+     */
+    public function addEleve(Eleve $eleve): self
     {
-        if (!$this->eleves->contains($elefe)) {
-            $this->eleves[] = $elefe;
-            $elefe->addIdClass($this);
+        if (!$this->eleves->contains($eleve)) {
+            $this->eleves[] = $eleve;
+            $eleve->addIdClass($this);
         }
 
         return $this;
     }
 
-    public function removeElefe(Eleve $elefe): self
+
+    /**
+     * Remove an Eleve Object to the Classe object.
+     * @param Eleve $eleve
+     * @return $this
+     */
+    public function removeEleve(Eleve $eleve): self
     {
-        if ($this->eleves->contains($elefe)) {
-            $this->eleves->removeElement($elefe);
-            $elefe->removeIdClass($this);
+        if ($this->eleves->contains($eleve)) {
+            $this->eleves->removeElement($eleve);
+            $eleve->removeIdClass($this);
         }
 
         return $this;
