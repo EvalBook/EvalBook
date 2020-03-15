3d437ac0c30bdd0cf774258a5c38d42f9bd21586
Nux007 <007.nux@gmail.com>
2020-03-15 07:58:35 +0100

    Translation system and translatable strings.

--- src/Controller/TestController.php
+++ src/Controller/TestController.php
@@ -11,6 +11,7 @@ use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
 use Symfony\Component\Security\Core\Encoder\UserPasswordEncoderInterface;
+use Symfony\Contracts\Translation\TranslatorInterface;
 
 class TestController extends AbstractController
 {
@@ -18,9 +19,10 @@ class TestController extends AbstractController
      * @Route("/test", name="test")
      * @param EntityManagerInterface $em
      * @param UserPasswordEncoderInterface $passwordEncoder
+     * @param TranslatorInterface $translator
      * @return Response
      */
-    public function index(EntityManagerInterface $em, UserPasswordEncoderInterface $passwordEncoder)
+    public function index(EntityManagerInterface $em, UserPasswordEncoderInterface $passwordEncoder, TranslatorInterface $translator)
     {
         /**
          * Création d'un utilisateur et sécurisation du mot de passe en utilisant la classe helper...
@@ -29,7 +31,10 @@ class TestController extends AbstractController
         // Création d'une secrétaire.
         $helper = new CommonUserHelper($em, $passwordEncoder);
         $result = $helper->createSecretary("first", "lastName", "admin@evalbook.dev", "Dev007!!");
-        $result ? $this->addFlash('success', 'User created') : $this->addFlash('error', 'User creation error');
+        if($result)
+            $this->addFlash('success', $translator->trans('User created'));
+        else
+            $this->addFlash('error', $translator->trans('User creation error'));
 
 
 
