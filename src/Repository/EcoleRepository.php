17bfc57db299b04c5f536b47421c5b25f27bf93a
Nux007 <007.nux@gmail.com>
2020-03-17 13:51:53 +0100

    Licence headers on php files.

--- src/Repository/EcoleRepository.php
+++ src/Repository/EcoleRepository.php
@@ -1,5 +1,22 @@
 <?php
 
+/**
+ * Copyleft (c) 2020 EvalBook
+ *
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the European Union Public Licence (EUPL V 1.2),
+ * version 1.2 (or any later version).
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * European Union Public Licence for more details.
+ *
+ * You should have received a copy of the European Union Public Licence
+ * along with this program. If not, see
+ * https://joinup.ec.europa.eu/collection/eupl/eupl-text-eupl-12
+ **/
+
 namespace App\Repository;
 
 use App\Entity\Ecole;
