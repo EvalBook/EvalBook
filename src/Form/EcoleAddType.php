6491efad6d0f76729b7211aee8e1ea2b3e698166
Nux007 <007.nux@gmail.com>
2020-03-24 11:53:01 +0100

    Added apache dependency & Started add school form.

---
+++ src/Form/EcoleAddType.php
@@ -0,0 +1,48 @@
+<?php
+
+namespace App\Form;
+
+use App\Entity\Ecole;
+use Symfony\Component\Form\AbstractType;
+use Symfony\Component\Form\FormBuilderInterface;
+use Symfony\Component\OptionsResolver\OptionsResolver;
+use Symfony\Contracts\Translation\TranslatorInterface;
+
+class EcoleAddType extends AbstractType
+{
+
+    private $translator;
+
+    /**
+     * UserRoleType constructor.
+     * @param TranslatorInterface $translator
+     */
+    public function __construct(TranslatorInterface $translator)
+    {
+        $this->translator = $translator;
+    }
+
+
+    /**
+     * Create a school add form.
+     * @param FormBuilderInterface $builder
+     * @param array $options
+     */
+    public function buildForm(FormBuilderInterface $builder, array $options)
+    {
+        $builder
+            ->add('name')
+        ;
+    }
+
+
+    /**
+     * @param OptionsResolver $resolver
+     */
+    public function configureOptions(OptionsResolver $resolver)
+    {
+        $resolver->setDefaults([
+            'data_class' => Ecole::class,
+        ]);
+    }
+}
