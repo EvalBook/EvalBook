9fbf4ade110e91cab882a9afe92c0ae46f453b32
Nux007 <007.nux@gmail.com>
2020-04-19 14:39:25 +0200

    Started Svelte integration.

---
+++ assets/js/App.svelte
@@ -0,0 +1,16 @@
+<script>
+    import { onMount } from 'svelte';
+
+    onMount(() => {
+        console.log('Hello, Symfony + Svelte!');
+    });
+</script>
+
+<h1>Symfony et Svelte</h1>
+
+<style>
+    h1 {
+        text-align: center;
+        color: #D26D26;
+    }
+</style>
