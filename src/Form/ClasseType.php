a8c2e1b5eccf387183e74f0505c746b0c79eedb4
Nux007 <007.nux@gmail.com>
2020-05-05 19:41:18 +0200

    Started classes.

---
+++ src/Form/ClasseType.php
@@ -0,0 +1,29 @@
+<?php
+
+namespace App\Form;
+
+use App\Entity\Classe;
+use Symfony\Component\Form\AbstractType;
+use Symfony\Component\Form\FormBuilderInterface;
+use Symfony\Component\OptionsResolver\OptionsResolver;
+
+class ClasseType extends AbstractType
+{
+    public function buildForm(FormBuilderInterface $builder, array $options)
+    {
+        $builder
+            ->add('name')
+            ->add('users')
+            ->add('eleves')
+            ->add('titulaire')
+            ->add('implantation')
+        ;
+    }
+
+    public function configureOptions(OptionsResolver $resolver)
+    {
+        $resolver->setDefaults([
+            'data_class' => Classe::class,
+        ]);
+    }
+}
