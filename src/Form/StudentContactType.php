9f917db516e7120646c78d0dabb0ee60476c8af9
Nux007 <007.nux@gmail.com>
2020-06-24 14:42:16 +0200

    Phone numbers validation.

--- src/Form/StudentContactType.php
+++ src/Form/StudentContactType.php
@@ -5,12 +5,12 @@ namespace App\Form;
 use Symfony\Component\Form\AbstractType;
 use Symfony\Component\Form\Extension\Core\Type\ChoiceType;
 use Symfony\Component\Form\Extension\Core\Type\EmailType;
-use Symfony\Component\Form\Extension\Core\Type\NumberType;
 use Symfony\Component\Form\Extension\Core\Type\SubmitType;
 use Symfony\Component\Form\Extension\Core\Type\TextType;
 use Symfony\Component\Form\FormBuilderInterface;
 use Symfony\Component\OptionsResolver\OptionsResolver;
 use Symfony\Component\Validator\Constraints\NotBlank;
+use Symfony\Component\Validator\Constraints\Regex;
 
 class StudentContactType extends AbstractType
 {
@@ -24,7 +24,15 @@ class StudentContactType extends AbstractType
 
             ->add('address', TextType::class)
 
-            ->add('phone', NumberType::class)
+            ->add('phone', TextType::class, [
+                'constraints' => [
+                    // Enable callback to check if start date is lower than period end date.
+                    new Regex([
+                        'message' => 'This phone number is invalid, it must match +32479111111 or +32 479 11 11 11 or +32479 11 11 11',
+                        'pattern' => '/\+?[0-9]{2,3}\s?[0-9\s]{3,}/',
+                    ]),
+                ]
+            ])
 
             ->add('email', EmailType::class)
 
