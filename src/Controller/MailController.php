a687fae32b96f2e722ae3a32fadff5fcb4b2b12f
Nux007 <007.nux@gmail.com>
2020-06-27 15:28:12 +0200

    Simple generic mail form.

--- src/Controller/MailController.php
+++ src/Controller/MailController.php
@@ -7,7 +7,11 @@ use App\Entity\User;
 use App\Form\MailType;
 use Sensio\Bundle\FrameworkExtraBundle\Configuration\ParamConverter;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\Mailer\Exception\TransportExceptionInterface;
+use Symfony\Component\Mailer\MailerInterface;
+use Symfony\Component\Mime\Email;
 use Symfony\Component\Routing\Annotation\Route;
 
 class MailController extends AbstractController
@@ -19,14 +23,51 @@ class MailController extends AbstractController
      *
      * @param User $user
      * @param StudentContact $studentContact
+     * @param Request $request
+     * @param MailerInterface $mailer
      * @return Response
      */
-    public function sendMail(User $user, StudentContact $studentContact)
+    public function sendMail(User $user, StudentContact $studentContact, Request $request, MailerInterface $mailer)
     {
         $mailForm = $this->createForm(MailType::class, null, [
             'from' => $user->getEmail(),
             'to' => $studentContact->getEmail(),
         ]);
+
+        $mailForm->handleRequest($request);
+
+        if($mailForm->isSubmitted() && $mailForm->isValid()) {
+            // Prepare email.
+            $email = (new Email())
+                ->from($mailForm->get('from')->getData())
+                ->addReplyTo($mailForm->get('from')->getData())
+                ->to(trim($mailForm->get('to')->getData()))
+                ->subject($mailForm->get('subject')->getData())
+                ->text($mailForm->get('message')->getData())
+                ->priority(Email::PRIORITY_HIGHEST)
+            ;
+
+            // Add attachement if any.
+            if($attachement = $mailForm->get('attachement')->getData()) {
+                $email->attach($attachement->getPathName(), $attachement->getClientOriginalName());
+            }
+
+            // Add Carbon copies.
+            if($carbonAddresses = $mailForm->get('carbonCopy')->getData()) {
+                $email->cc(...array_map( function($address) {
+                    return trim($address);
+                }, explode(',', $carbonAddresses)));
+            }
+
+            // Send mail.
+            try {
+                $mailer->send($email);
+                $this->addFlash('success', 'Your mail was sent !');
+            }
+            catch (TransportExceptionInterface $transportError) {
+                $this->addFlash('error', 'Your mail was not sent, an error occurred.');
+            }
+        }
 
         return $this->render('mail/index.html.twig', [
             'mailForm' => $mailForm->createView(),
--- src/Form/MailType.php
+++ src/Form/MailType.php
@@ -10,6 +10,9 @@ use Symfony\Component\Form\Extension\Core\Type\TextareaType;
 use Symfony\Component\Form\Extension\Core\Type\TextType;
 use Symfony\Component\Form\FormBuilderInterface;
 use Symfony\Component\OptionsResolver\OptionsResolver;
+use Symfony\Component\Validator\Constraints\Callback;
+use Symfony\Component\Validator\Constraints\NotBlank;
+use Symfony\Component\Validator\Context\ExecutionContextInterface;
 
 class MailType extends AbstractType
 {
@@ -24,13 +27,67 @@ class MailType extends AbstractType
                 'data' => $options['to'],
             ])
 
-            ->add('cc', TextType::class)
+            ->add('carbonCopy', TextType::class, [
+                'required' => false,
+                'constraints' => new Callback(function($object, ExecutionContextInterface $context) {
+                    $carbon = $context->getRoot()->get('carbonCopy')->getData();
+                    if(is_null($carbon)) {
+                        return;
+                    }
+                    elseif(strpos($carbon, ',') !== -1) {
+                        foreach (explode(',', $carbon) as $cc) {
+                            if (!filter_var(trim($cc), FILTER_VALIDATE_EMAIL)) {
+                                $context->buildViolation('A mail address in carbon copy is not valid')
+                                        ->addViolation();
+                            }
+                        }
+                    }
+                    else {
+                        if (!filter_var(trim($carbon), FILTER_VALIDATE_EMAIL)) {
+                            $context->buildViolation('A mail address in carbon copy is not valid')
+                                    ->addViolation();
+                        }
+                    }
+                }),
+            ])
 
-            ->add('subject', TextType::class)
+            ->add('subject', TextType::class, [
+                'constraints' => new NotBlank([
+                   'message' => 'The mail subject can not be empty'
+                ]),
+            ])
 
-            ->add('message', TextareaType::class)
+            ->add('message', TextareaType::class, [
+                'constraints' => new NotBlank([
+                    'message' => 'The mail content can not be empty'
+                ]),
+            ])
 
-            ->add('attachement', FileType::class)
+            ->add('attachement', FileType::class, [
+                'required' => false,
+                'constraints' => new Callback(function($object, ExecutionContextInterface $context) {
+                    $file = $context->getRoot()->get('attachement')->getData();
+                    if(is_null($file))
+                        return;
+
+                    if(in_array($file->getClientMimeType(), [
+                        'application/javascript',
+                        'application/octet-stream',
+                        'application/octet-stream',
+                        'application/x-msdownload',
+                        'application/exe',
+                        'application/x-exe',
+                        'application/dos-exe',
+                        'vms/exe',
+                        'application/x-winexe',
+                        'application/msdos-windows',
+                        'application/x-msdos-program',
+                    ])) {
+                        $context->buildViolation('The file type is not allowed')
+                                ->addViolation();
+                    }
+                }),
+            ])
 
             ->add('submit', SubmitType::class)
         ;
