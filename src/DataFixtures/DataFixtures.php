7bf6d07bd2fc6180b0d7a3885081abbaa5076d09
Nux007 <007.nux@gmail.com>
2020-07-13 19:43:42 +0200

    Added licence headers.

--- src/DataFixtures/DataFixtures.php
+++ src/DataFixtures/DataFixtures.php
@@ -1,5 +1,22 @@
 <?php
 
+/**
+ * Copyleft (c) 2020 EvalBook
+ *
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the European Union Public Licence (EUPL V 1.2),
+ * version 1.2 (or any later version).
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * European Union Public Licence for more details.
+ *
+ * You should have received a copy of the European Union Public Licence
+ * along with this program. If not, see
+ * https://joinup.ec.europa.eu/collection/eupl/eupl-text-eupl-12
+ **/
+
 namespace App\DataFixtures;
 
 use App\Entity\Activity;
@@ -11,7 +28,6 @@ use App\Entity\Note;
 use App\Entity\NoteType;
 use App\Entity\Period;
 use App\Entity\User;
-use App\Repository\SchoolRepository;
 use Doctrine\Bundle\FixturesBundle\Fixture;
 use Doctrine\Common\DataFixtures\FixtureInterface;
 use Doctrine\Common\DataFixtures\OrderedFixtureInterface;
