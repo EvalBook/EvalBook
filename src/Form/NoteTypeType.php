ddf6d4547ba088e6dfc5ee69d0b7be2b0eb2555a
Nux007 <007.nux@gmail.com>
2020-07-23 11:54:05 +0200

    New custom note type view.

--- src/Form/NoteTypeType.php
+++ src/Form/NoteTypeType.php
@@ -4,20 +4,44 @@ namespace App\Form;
 
 use App\Entity\NoteType;
 use Symfony\Component\Form\AbstractType;
+use Symfony\Component\Form\Extension\Core\Type\SubmitType;
+use Symfony\Component\Form\Extension\Core\Type\TextType;
 use Symfony\Component\Form\FormBuilderInterface;
 use Symfony\Component\OptionsResolver\OptionsResolver;
+use Symfony\Component\Validator\Constraints\Length;
 
 class NoteTypeType extends AbstractType
 {
     public function buildForm(FormBuilderInterface $builder, array $options)
     {
         $builder
-            ->add('name')
+            // Note type name.
+            ->add('name', TextType::class, [
+                'constraints' => new Length([
+                    'min' => 1,
+                    'max' => 255,
+                    'minMessage' => "noteType.name-too-short",
+                    'maxMessage' => "noteType.name-too-long",
+                ])
+            ])
+
+            // Note type description.
             ->add('description')
+
+            // Minimum allowable note.
             ->add('minimum')
-            ->add('interval')
+
+            // Note type intervals.
+            ->add('intervals')
+
+            // Maximum allowable note.
             ->add('maximum')
+
+            // Note coefficient.
             ->add('coefficient')
+
+            // Submit button.
+            ->add('submit', SubmitType::class)
         ;
     }
 
