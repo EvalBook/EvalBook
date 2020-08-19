a777ec98f6a9a80f52b6f2ebf339135760b514bd
pickforbass <56023752+pickforbass@users.noreply.github.com>
2020-08-19 15:21:52 +0200

    set modalSchoolReport.js
    correct minor css bugs

--- assets/js/modalSchoolReport.js
+++ assets/js/modalSchoolReport.js
@@ -8,7 +8,22 @@ let ModalSchoolReport = function(data) {
      * Display the school report.
      */
     this.display = function() {
-        console.log(this.data);
+        let frame = document.createElement('div');
+        frame.setAttribute('id',"modal-window");
+        frame.setAttribute('aria-modal',"config");
+        document.body.appendChild(frame);
+        frame = document.getElementById('modal-window');
+
+        let close = document.createElement('span');
+        close.innerHTML = "Fermer cette fenêtre";
+        close.setAttribute('id','closing-span');
+        close.setAttribute('class','w-100 d-flex align-items-end');
+        frame.appendChild(close);
+
+        let view = document.createElement('div');
+        view.setAttribute('class',"w-100 d-flex align-items-end");
+        view.setAttribute('id',"report-view");
+        frame.appendChild(view);
     }
 }
 
