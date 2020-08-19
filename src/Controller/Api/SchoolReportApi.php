7b628f5bfc4773d0e05580503336b275a37d7b2f
Nux007 <007.nux@gmail.com>
2020-08-19 15:44:18 +0200

    School report next.

--- src/Controller/Api/SchoolReportApi.php
+++ src/Controller/Api/SchoolReportApi.php
@@ -21,8 +21,15 @@ namespace App\Controller\Api;
 
 header("Access-Control-Allow-Origin: *");
 
+use App\Entity\ActivityTheme;
+use App\Entity\ActivityThemeDomain;
+use App\Entity\ActivityThemeDomainSkill;
+use App\Entity\Implantation;
 use App\Entity\Note;
+use App\Entity\Period;
 use App\Entity\Student;
+use App\Repository\PeriodRepository;
+use Sensio\Bundle\FrameworkExtraBundle\Configuration\ParamConverter;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\JsonResponse;
 use Symfony\Component\Routing\Annotation\Route;
@@ -31,21 +38,31 @@ use Symfony\Component\Routing\Annotation\Route;
 class SchoolReportApi extends AbstractController
 {
     /**
-     * @Route("/school/report/individual/{student}", name="school_report_individual")
+     * @Route("/school/report/individual/{student}/{implantation}", name="school_report_individual")
+     * @ParamConverter("student", class="App\Entity\Student")
+     * @ParamConverter("implantation", class="App\Entity\Implantation")
+     *
      * @param Student $student
+     * @param Implantation $implantation
+     * @param PeriodRepository $periodRepository
+     * @return JsonResponse
      */
-    public function getIndividualSchoolReport(Student $student)
-    {
-        $notes = $student->getNotes()->toArray();
-        $notes = array_filter($notes, function(Note $note){
-            $period = $note->getActivity()->getPeriod();
-            $now = date('now');
-            // if end date >= now and start_date <= now
-            return $period->getDateEnd() >= $now && $period->getDateStart() <= $now;
+    public function getIndividualSchoolReport(Student $student, Implantation $implantation, PeriodRepository $periodRepository){
+        $notes = [];
+
+        // Getting closed periods.
+        $periods = $implantation->getPeriods();
+        $periods = array_filter($periods->toArray(), function(Period $period) {
+            return $period->getDateStart() < new \DateTime('now');
         });
 
-        dd($notes);
-        $result = [];
+        // Filtering notes by periods.
+        foreach($student->getNotes() as $note) {
+            if(in_array($note->getActivity()->getPeriod()->getId(), array_map(function(Period $period){return $period->getId();}, $periods))) {
+                $notes[] = $note;
+            }
+        }
+
         if(count($notes) > 0) {
             $result = $this->compute($notes);
         }
@@ -65,9 +82,37 @@ class SchoolReportApi extends AbstractController
     /**
      * Compute the student notes.
      * @param array $notes
+     * @return array
      */
     private function compute(array $notes)
     {
+        $themeRepository  = $this->getDoctrine()->getRepository(ActivityTheme::class);
+        $domainRepository = $this->getDoctrine()->getRepository(ActivityThemeDomain::class);
+        $skillRepository  = $this->getDoctrine()->getRepository(ActivityThemeDomainSkill::class);
 
+        $result = [];
+        foreach($notes as $note) {
+            /* @var $note Note */
+            // Getting target theme.
+            $theme = $themeRepository->findOneBy([
+                'id' => $note->getActivity()->getActivityThemeDomainSkill()->getActivityThemeDomain()->getActivityTheme()
+            ]);
+
+            // Getting target domain.
+            $domain = $domainRepository->findOneBy([
+                'id' => $note->getActivity()->getActivityThemeDomainSkill()->getActivityThemeDomain()
+            ]);
+
+            // Getting target skill.
+            $skill = $skillRepository->findOneBy([
+                'id' => $note->getActivity()->getActivityThemeDomainSkill()
+            ]);
+
+            $result[$theme->getDisplayName()][$domain->getDisplayName()][$skill->getName()][] = $note;
+        }
+
+        dd($result);
+
+        return $result;
     }
 }
